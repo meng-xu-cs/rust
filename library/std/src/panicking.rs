fc7c3aae7f24c2908a0691b3aa076c659aaf954f
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-05-07 16:51:59 +0200

    [SOL] Enable test harness for BPF programs

--- library/std/src/panicking.rs
+++ library/std/src/panicking.rs
@@ -44,7 +44,6 @@ use crate::{intrinsics, process, thread};
 // One day this may look a little less ad-hoc with the compiler helping out to
 // hook up these functions, but it is not this day!
 #[allow(improper_ctypes)]
-#[cfg(not(target_arch = "bpf"))]
 extern "C" {
     fn __rust_panic_cleanup(payload: *mut u8) -> *mut (dyn Any + Send + 'static);
 }
@@ -152,6 +151,12 @@ pub fn set_hook(hook: Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send>) {
     drop(old);
 }
 
+/// Dummy version for satisfying library/test dependencies for BPF target
+#[cfg(target_arch = "bpf")]
+#[stable(feature = "panic_hooks", since = "1.10.0")]
+pub fn set_hook(_hook: Box<dyn Fn(&PanicInfo<'_>) + 'static + Sync + Send>) {
+}
+
 /// Unregisters the current panic hook and returns it, registering the default hook
 /// in its place.
 ///
@@ -243,6 +248,13 @@ where
     *hook = Hook::Custom(Box::new(move |info| hook_fn(&prev, info)));
 }
 
+/// Dummy version for satisfying library/test dependencies for BPF target
+#[cfg(target_arch = "bpf")]
+#[stable(feature = "panic_hooks", since = "1.10.0")]
+pub fn take_hook() -> Box<dyn Fn(&PanicInfo<'_>) + 'static + Sync + Send> {
+    Box::new(default_hook)
+}
+
 /// The default panic handler.
 #[optimize(size)]
 #[cfg(not(target_arch = "bpf"))]
@@ -310,7 +322,11 @@ fn default_hook(info: &PanicHookInfo<'_>) {
     }
 }
 
-#[cfg(all(not(test), not(target_arch = "bpf")))]
+#[cfg(target_arch = "bpf")]
+fn default_hook(_info: &PanicInfo<'_>) {
+}
+
+#[cfg(not(test))]
 #[doc(hidden)]
 #[cfg(feature = "panic_immediate_abort")]
 #[unstable(feature = "update_panic_count", issue = "none")]
@@ -405,6 +421,7 @@ pub mod panic_count {
     //
     // This also updates thread-local state to keep track of whether a panic
     // hook is currently executing.
+    #[cfg(not(target_arch = "bpf"))]
     pub fn increase(run_panic_hook: bool) -> Option<MustAbort> {
         let global_count = GLOBAL_PANIC_COUNT.fetch_add(1, Ordering::Relaxed);
         if global_count & ALWAYS_ABORT_FLAG != 0 {
@@ -443,12 +460,14 @@ pub mod panic_count {
 
     // Disregards ALWAYS_ABORT_FLAG
     #[must_use]
+    #[cfg(not(target_arch = "bpf"))]
     pub fn get_count() -> usize {
         LOCAL_PANIC_COUNT.with(|c| c.get().0)
     }
 
     // Disregards ALWAYS_ABORT_FLAG
     #[must_use]
+    #[cfg(not(target_arch = "bpf"))]
     #[inline]
     pub fn count_is_zero() -> bool {
         if GLOBAL_PANIC_COUNT.load(Ordering::Relaxed) & !ALWAYS_ABORT_FLAG == 0 {
@@ -469,6 +488,7 @@ pub mod panic_count {
 
     // Slow path is in a separate function to reduce the amount of code
     // inlined from `count_is_zero`.
+    #[cfg(not(target_arch = "bpf"))]
     #[inline(never)]
     #[cold]
     fn is_zero_slow_path() -> bool {
@@ -476,7 +496,7 @@ pub mod panic_count {
     }
 }
 
-#[cfg(all(test, not(target_arch = "bpf")))]
+#[cfg(test)]
 pub use realstd::rt::panic_count;
 
 /// Invoke a closure, capturing the cause of an unwinding panic if one occurs.
