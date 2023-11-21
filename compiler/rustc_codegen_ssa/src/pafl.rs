47c19cd1528048c68f064ee9edcdf008fb0cc248
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-11-20 20:55:36 -0400

    [fuzz] trim down the types of codegen instances recorded

--- compiler/rustc_codegen_ssa/src/pafl.rs
+++ compiler/rustc_codegen_ssa/src/pafl.rs
@@ -24,7 +24,7 @@ pub fn dump(tcx: TyCtxt<'_>, outdir: &Path) {
     fs::create_dir_all(&path_data).expect("unable to create meta directory");
 
     // extract the mir for each codegen unit
-    let mut summary = CrateSummary { natives: Vec::new(), functions: Vec::new() };
+    let mut summary = CrateSummary { functions: Vec::new() };
 
     let (_, units) = tcx.collect_and_partition_mono_items(());
     for unit in units {
@@ -83,32 +83,14 @@ pub fn dump(tcx: TyCtxt<'_>, outdir: &Path) {
                 InstanceDef::Item(id) => {
                     summary.functions.push(FunctionSummary::process(tcx, *id));
                 }
-                InstanceDef::Intrinsic(id) => {
-                    summary.natives.push(NativeSummary::process(tcx, *id, NativeKind::Intrinsic));
-                }
-                InstanceDef::ClosureOnceShim { call_once: id, track_caller: _ } => {
-                    summary.natives.push(NativeSummary::process(tcx, *id, NativeKind::Once));
-                }
-                InstanceDef::DropGlue(id, _) => {
-                    summary.natives.push(NativeSummary::process(tcx, *id, NativeKind::Drop));
-                }
-                InstanceDef::CloneShim(id, _) => {
-                    summary.natives.push(NativeSummary::process(tcx, *id, NativeKind::Clone));
-                }
-                InstanceDef::Virtual(id, index) => {
-                    summary.natives.push(NativeSummary::process(
-                        tcx,
-                        *id,
-                        NativeKind::Virtual(*index),
-                    ));
-                }
-                InstanceDef::VTableShim(id) => {
-                    summary.natives.push(NativeSummary::process(tcx, *id, NativeKind::VTable));
-                }
-                InstanceDef::FnPtrShim(id, _) => {
-                    summary.natives.push(NativeSummary::process(tcx, *id, NativeKind::FnPtr));
-                }
-                InstanceDef::ReifyShim(..)
+                InstanceDef::Intrinsic(..)
+                | InstanceDef::ClosureOnceShim { .. }
+                | InstanceDef::DropGlue(..)
+                | InstanceDef::CloneShim(..)
+                | InstanceDef::Virtual(..)
+                | InstanceDef::VTableShim(..)
+                | InstanceDef::FnPtrShim(..)
+                | InstanceDef::ReifyShim(..)
                 | InstanceDef::FnPtrAddrShim(..)
                 | InstanceDef::ThreadLocalShim(..) => {
                     bug!("unusual calls are not supported yet: {}", instance);
@@ -147,36 +129,7 @@ impl From<DefId> for Ident {
 /// A struct containing serializable information about the entire crate
 #[derive(Serialize)]
 struct CrateSummary {
-    natives: Vec<NativeSummary>,
     functions: Vec<FunctionSummary>,
-}
-
-/// Kinds of native constructs
-#[derive(Serialize)]
-enum NativeKind {
-    Intrinsic,
-    Once,
-    Drop,
-    Clone,
-    Virtual(usize),
-    VTable,
-    FnPtr,
-}
-
-/// A struct containing serializable information about one native function
-#[derive(Serialize)]
-struct NativeSummary {
-    id: Ident,
-    path: String,
-    kind: NativeKind,
-}
-
-impl NativeSummary {
-    /// Process an intrinsic instance
-    fn process<'tcx>(tcx: TyCtxt<'tcx>, id: DefId, kind: NativeKind) -> Self {
-        let path = tcx.def_path(id).to_string_no_crate_verbose();
-        Self { id: id.into(), path, kind }
-    }
 }
 
 /// A struct containing serializable information about one user-defined function
