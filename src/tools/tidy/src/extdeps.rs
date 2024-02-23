5e95b1dc4c9379ae4c3d338b3c253e570f0bbb96
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2024-02-23 10:02:59 -0500

    [SOL] Update references to solana-labs redirecting them to anza-xyz

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -8,7 +8,7 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
-    "\"git+https://github.com/solana-labs/compiler-builtins?tag=solana-tools-v1.40#81ef46f3fe1357095acdd089a700890fe4e13974\""
+    "\"git+https://github.com/anza-xyz/compiler-builtins?tag=solana-tools-v1.40#81ef46f3fe1357095acdd089a700890fe4e13974\""
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
