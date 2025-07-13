95808e1ffb7f33e79fd3ae3143e603b0f991dc9d
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-07-13 13:21:00 -0400

    information dumping

--- compiler/rustc_codegen_ssa/src/solana/mod.rs
+++ compiler/rustc_codegen_ssa/src/solana/mod.rs
@@ -2,7 +2,9 @@ use std::env;
 use std::path::PathBuf;
 
 use rustc_middle::bug;
+use rustc_middle::mir::mono::MonoItem;
 use rustc_middle::ty::TyCtxt;
+use rustc_span::FileNameDisplayPreference;
 use tracing::info;
 
 const COMPONENT_NAME: &str = "solanalysis";
@@ -10,7 +12,7 @@ const COMPONENT_NAME: &str = "solanalysis";
 /// Entrypoint fo the solana-specific logic
 pub(crate) fn solanalysis_entrypoint(tcx: TyCtxt<'_>) {
     // grab information from the environment variables
-    let outdir = match env::var_os(COMPONENT_NAME.to_uppercase()) {
+    let _outdir = match env::var_os(COMPONENT_NAME.to_uppercase()) {
         None => return,
         Some(val) => PathBuf::from(val),
     };
@@ -20,23 +22,42 @@ pub(crate) fn solanalysis_entrypoint(tcx: TyCtxt<'_>) {
         Some(val) => PathBuf::from(val),
     };
 
-    let src_path = match tcx.sess.local_crate_source_file() {
+    let (src_file_name, src_path_full) = match tcx.sess.local_crate_source_file() {
         None => bug!("unable to locate local crate source file"),
-        Some(src) => match src.into_local_path() {
-            None => bug!("local crate source file is not a local path"),
-            Some(path) => path,
-        },
+        Some(src) => {
+            let file_name = src.to_path(FileNameDisplayPreference::Local).to_path_buf();
+            let full_path = src.local_path_if_available().canonicalize().unwrap_or_else(|e| {
+                bug!("failed to canonicalize source path for {}: {e}", file_name.display())
+            });
+            (file_name, full_path)
+        }
     };
 
     // skip execution on source files that are not under the target prefix
-    if !src_path.starts_with(&prefix) {
+    if !src_path_full.starts_with(&prefix) {
         return;
     }
 
     // now enters the actual workflow
-    info!(
-        "{COMPONENT_NAME} enabled for {} with output director: {}",
-        src_path.display(),
-        outdir.display()
-    );
+    info!("running {COMPONENT_NAME} for {}", src_file_name.display());
+
+    let partitions = tcx.collect_and_partition_mono_items(());
+    for unit in partitions.codegen_units {
+        info!("  -> codegen unit {}", unit.name());
+        for (k, _v) in unit.items_in_deterministic_order(tcx) {
+            match k {
+                MonoItem::Fn(instance) => {
+                    let id = instance.def_id();
+                    info!("    - instance[{}]: {}", id.is_local(), tcx.def_path_str(id));
+                }
+                MonoItem::Static(id) => {
+                    info!("    - static[{}]: {}", id.is_local(), tcx.def_path_str(id));
+                }
+                MonoItem::GlobalAsm(_) => {
+                    bug!("[assumption] unexpected assembly item in codegen unit")
+                }
+            }
+        }
+        info!("  <- codegen unit {}", unit.name());
+    }
 }
