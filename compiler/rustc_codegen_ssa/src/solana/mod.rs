b4f10f8557ff9b937e8ed3f754b6d6dbe8322dbf
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-07-13 10:41:11 -0400

    solanalysis development

---
+++ compiler/rustc_codegen_ssa/src/solana/mod.rs
@@ -0,0 +1,29 @@
+use rustc_middle::ty::{self, Instance, TyCtxt};
+
+pub(crate) mod common;
+pub(crate) mod pipeline_anchor;
+
+/// Entrypoint for the solana-specific codegen logic
+pub(crate) fn entrypoint<'tcx>(tcx: TyCtxt<'tcx>, instance: Instance<'tcx>) {
+    let sol = match common::retrieve_context(tcx) {
+        None => return,
+        Some(ctxt) => ctxt,
+    };
+
+    let instance_mir = tcx.instance_mir(instance.def).clone();
+    let mut monomorphised_mir = instance.instantiate_mir_and_normalize_erasing_regions(
+        tcx,
+        ty::TypingEnv::fully_monomorphized(),
+        ty::EarlyBinder::bind(instance_mir),
+    );
+
+    match sol.build_system {
+        common::BuildSystem::Anchor => match sol.phase {
+            common::Phase::Bootstrap => {
+                pipeline_anchor::phase_bootstrap(tcx, sol, &mut monomorphised_mir)
+            }
+        },
+    }
+
+    // FIXME: return the monomorphised MIR for further processing
+}
