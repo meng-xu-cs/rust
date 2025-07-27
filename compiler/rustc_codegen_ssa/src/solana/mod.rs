aa79680eb8ac22a47c719f387fd919e7e8a4fda7
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-07-26 21:51:53 -0300

    reorg

--- compiler/rustc_codegen_ssa/src/solana/mod.rs
+++ compiler/rustc_codegen_ssa/src/solana/mod.rs
@@ -1,7 +1,7 @@
-use rustc_middle::mir::{MirPhase, RuntimePhase};
-use rustc_middle::ty::{self, Instance, TyCtxt};
+use rustc_middle::ty::{Instance, TyCtxt};
 
 pub(crate) mod common;
+pub(crate) mod function;
 pub(crate) mod ident;
 pub(crate) mod typing;
 
@@ -14,27 +14,12 @@ pub(crate) fn entrypoint<'tcx>(tcx: TyCtxt<'tcx>, instance: Instance<'tcx>) {
         Some(ctxt) => ctxt,
     };
 
-    // skip shims and other non-user defined items
-    let instance_mir = tcx.instance_mir(instance.def);
-    if !matches!(instance_mir.phase, MirPhase::Runtime(RuntimePhase::Optimized)) {
-        return;
-    }
-
-    // convert the instance to monomorphised MIR
-    let mut monomorphised_mir = instance.instantiate_mir_and_normalize_erasing_regions(
-        tcx,
-        instance_mir.typing_env(tcx),
-        ty::EarlyBinder::bind(instance_mir.clone()),
-    );
-
     // work on the monomorphised MIR
     match sol.build_system {
         common::BuildSystem::Anchor => match sol.phase {
             common::Phase::Bootstrap => {
-                pipeline_anchor::phase_bootstrap(tcx, sol, &mut monomorphised_mir)
+                pipeline_anchor::phase_bootstrap(tcx, sol, instance);
             }
         },
     }
-
-    // FIXME: return the monomorphised MIR for further processing
 }
