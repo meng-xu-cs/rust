ffcb3afb4a8c8f6f952c2c8942ffe00cc5471db8
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-08-03 22:42:33 -0400

    x

--- compiler/rustc_codegen_ssa/src/solana/builtin.rs
+++ compiler/rustc_codegen_ssa/src/solana/builtin.rs
@@ -1,9 +1,8 @@
-use std::fmt::Display;
-
 use tracing::warn;
 
 use crate::solana::context::SolIdent;
 
+#[derive(Debug, Clone, Copy)]
 pub(crate) enum BuiltinFunction {
     /// core::intrinsics::abort
     IntrinsicsAbort,
@@ -11,32 +10,91 @@ pub(crate) enum BuiltinFunction {
     IntrinsicsColdPath,
     /// core::intrinsics::raw_eq
     IntrinsicsRawEq,
+
     /// core::hint::assert_unchecked::precondition_check
     HintAssertPreconditionCheck,
     /// core::alloc::layout::|self|::from_size_align_unchecked::precondition_check
-    AllocFromSizeAlignPreconditionCheck,
+    /// AllocFromSizeAlignPreconditionCheck,
     /// core::ptr::non_null::|self|::new_unchecked::precondition_check
-    PtrNonNullNewPreconditionCheck,
+    /// PtrNonNullNewPreconditionCheck,
     /// core::ptr::copy_nonoverlapping::precondition_check
-    CopyNonOverlappingPreconditionCheck,
+    /// CopyNonOverlappingPreconditionCheck,
+    /// alloc::vec::|self|::set_len:::precondition_check
+    /// VecSetLenPreconditionCheck,
+    #[allow(dead_code)] // TODO: remove this
+    X,
 }
 
 impl BuiltinFunction {
-    fn try_to_match(ident: &SolIdent, mut segments: Vec<&'static str>) -> bool {
-        let segment = match segments.pop() {
-            None => return false,
-            Some(s) => s,
-        };
-        match ident {
-            SolIdent::CrateRoot(name) => segments.is_empty() && name == segment,
-            SolIdent::FuncNs { parent, name } if name == segment => {
-                Self::try_to_match(parent, segments)
+    fn ident(&self) -> SolIdent {
+        match self {
+            Self::IntrinsicsAbort => {
+                SolIdent::from_root("core").with_type_ns("intrinsics").with_func_ns("abort")
             }
-            SolIdent::TypeNs { parent, name } if name == segment => {
-                Self::try_to_match(parent, segments)
+            Self::IntrinsicsColdPath => {
+                SolIdent::from_root("core").with_type_ns("intrinsics").with_func_ns("cold_path")
             }
-            SolIdent::SelfImpl { parent } if segment == "|self|" => {
-                Self::try_to_match(parent, segments)
+            Self::IntrinsicsRawEq => {
+                SolIdent::from_root("core").with_type_ns("intrinsics").with_func_ns("raw_eq")
+            }
+
+            Self::HintAssertPreconditionCheck => SolIdent::from_root("core")
+                .with_type_ns("hint")
+                .with_func_ns("assert_unchecked")
+                .with_func_ns("precondition_check"),
+            Self::X => SolIdent::from_root("<X>"), // TODO: remove
+        }
+    }
+
+    fn all_items() -> Vec<Self> {
+        vec![
+            Self::IntrinsicsAbort,
+            Self::IntrinsicsColdPath,
+            Self::IntrinsicsRawEq,
+            Self::HintAssertPreconditionCheck,
+        ]
+    }
+}
+
+// TODO: remove the mark
+#[allow(dead_code)]
+impl BuiltinFunction {
+    fn string_match(ident: &str, target: &str) -> bool {
+        target == "*" || ident == target
+    }
+
+    fn ident_match(ident: &SolIdent, target: &SolIdent) -> bool {
+        if matches!(target, SolIdent::CrateRoot(symbol) if symbol == "**") {
+            return true;
+        }
+        match (ident, target) {
+            (SolIdent::CrateRoot(name), SolIdent::CrateRoot(target_name)) => {
+                Self::string_match(name, target_name)
+            }
+            (
+                SolIdent::FuncNs { parent, name },
+                SolIdent::FuncNs { parent: target_parent, name: target_name },
+            ) => Self::string_match(name, target_name) && Self::ident_match(parent, target_parent),
+            (
+                SolIdent::TypeNs { parent, name },
+                SolIdent::TypeNs { parent: target_parent, name: target_name },
+            ) => Self::string_match(name, target_name) && Self::ident_match(parent, target_parent),
+            (
+                SolIdent::SelfImpl { parent, self_ident },
+                SolIdent::SelfImpl { parent: target_parent, self_ident: target_self_ident },
+            ) => {
+                Self::ident_match(parent, target_parent)
+                    && Self::ident_match(self_ident, target_self_ident)
+            }
+            (
+                SolIdent::TraitImpl { parent, trait_ident },
+                SolIdent::TraitImpl { parent: target_parent, trait_ident: target_trait_ident },
+            ) => {
+                Self::ident_match(parent, target_parent)
+                    && Self::ident_match(trait_ident, target_trait_ident)
+            }
+            (SolIdent::Extern { parent }, SolIdent::Extern { parent: target_parent }) => {
+                Self::ident_match(parent, target_parent)
             }
             _ => false,
         }
@@ -44,64 +102,11 @@ impl BuiltinFunction {
 
     pub(crate) fn try_to_resolve(ident: &SolIdent) -> Option<Self> {
         warn!("trying to resolve builtin function: {ident:#?}");
-
-        if Self::try_to_match(ident, vec!["core", "intrinsics", "abort"]) {
-            Some(Self::IntrinsicsAbort)
-        } else if Self::try_to_match(ident, vec!["core", "intrinsics", "cold_path"]) {
-            Some(Self::IntrinsicsColdPath)
-        } else if Self::try_to_match(ident, vec!["core", "intrinsics", "raw_eq"]) {
-            Some(Self::IntrinsicsRawEq)
-        } else if Self::try_to_match(
-            ident,
-            vec!["core", "hint", "assert_unchecked", "precondition_check"],
-        ) {
-            Some(Self::HintAssertPreconditionCheck)
-        } else if Self::try_to_match(
-            ident,
-            vec![
-                "core",
-                "alloc",
-                "layout",
-                "|self|",
-                "from_size_align_unchecked",
-                "precondition_check",
-            ],
-        ) {
-            Some(Self::AllocFromSizeAlignPreconditionCheck)
-        } else if Self::try_to_match(
-            ident,
-            vec!["core", "ptr", "non_null", "|self|", "new_unchecked", "precondition_check"],
-        ) {
-            Some(Self::PtrNonNullNewPreconditionCheck)
-        } else if Self::try_to_match(
-            ident,
-            vec!["core", "ptr", "copy_nonoverlapping", "precondition_check"],
-        ) {
-            Some(Self::CopyNonOverlappingPreconditionCheck)
-        } else {
-            None
-        }
-    }
-}
-
-impl Display for BuiltinFunction {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::IntrinsicsAbort => write!(f, "abort"),
-            Self::IntrinsicsColdPath => write!(f, "cold_path"),
-            Self::IntrinsicsRawEq => write!(f, "raw_eq"),
-            Self::HintAssertPreconditionCheck => {
-                write!(f, "hint::assert::precondition_check")
-            }
-            Self::AllocFromSizeAlignPreconditionCheck => {
-                write!(f, "alloc::from_size_align::precondition_check")
-            }
-            Self::PtrNonNullNewPreconditionCheck => {
-                write!(f, "non_null::new::precondition_check")
-            }
-            Self::CopyNonOverlappingPreconditionCheck => {
-                write!(f, "copy_nonoverlapping::precondition_check")
+        for item in Self::all_items() {
+            if Self::ident_match(ident, &item.ident()) {
+                return Some(item);
             }
         }
+        None
     }
 }
