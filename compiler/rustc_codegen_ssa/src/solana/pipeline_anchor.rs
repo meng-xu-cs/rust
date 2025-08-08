492275e4bf2f5771cb27748298f40d8caddf4dc3
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-08-07 21:05:04 -0700

    per-kind filter

--- compiler/rustc_codegen_ssa/src/solana/pipeline_anchor.rs
+++ compiler/rustc_codegen_ssa/src/solana/pipeline_anchor.rs
@@ -7,7 +7,7 @@ use rustc_middle::{bug, ty};
 use tracing::{info, warn};
 
 use crate::solana::common::SolEnv;
-use crate::solana::context::{SolContextBuilder, SolDeps};
+use crate::solana::context::{SolContextBuilder, SolDeps, SolInstanceKind};
 
 pub(crate) fn phase_bootstrap<'tcx>(tcx: TyCtxt<'tcx>, sol: SolEnv, instance: Instance<'tcx>) {
     info!(
@@ -122,15 +122,6 @@ pub(crate) fn phase_expansion<'tcx>(tcx: TyCtxt<'tcx>, sol: SolEnv, instance: In
     let def_desc = tcx.def_path_str_with_args(def_id, instance.args);
     info!("processing {def_desc}");
 
-    // a dependency must be a `fn` or `assoc fn` item
-    match instance.def {
-        InstanceKind::Item(did) if matches!(tcx.def_kind(did), DefKind::Fn | DefKind::AssocFn) => {}
-        _ => {
-            info!("- skipped, not a fn-alike item");
-            return;
-        }
-    };
-
     // load the deps
     let path = sol.prev_phase_output_dir().join("deps.json");
     let content = fs::read_to_string(path).unwrap_or_else(|e| {
@@ -143,23 +134,73 @@ pub(crate) fn phase_expansion<'tcx>(tcx: TyCtxt<'tcx>, sol: SolEnv, instance: In
     let inst_ident = SolContextBuilder::mk_ident_no_cache(tcx, def_id);
 
     // compare with the dependencies
-    let mut builder = SolContextBuilder::new(tcx, sol);
     let mut matched = false;
-    for (_, dep_ident, dep_args, _) in &deps.fn_deps {
+    for (dep_kind, dep_ident, dep_args, _) in &deps.fn_deps {
         if dep_ident != &inst_ident {
             continue;
         }
 
-        // now build the context around this instance
-        warn!("- found dependency {def_desc}");
-        let (_, def_ident, def_args) = builder.make_instance(instance);
+        // attempt to resolve the instance
+        info!("- attempting dependency {def_desc}");
+        let mut temp_builder = SolContextBuilder::new(tcx, sol.with_temporary_phase());
 
-        // now also check if the ident and type arguments match
-        if dep_ident != &def_ident {
-            bug!("[invariant] dependency ident mismatch on {def_desc}");
+        // try to match the type arguments
+        if dep_args.len() != instance.args.len() {
+            info!("- skipped, type argument count mismatch");
+            continue;
         }
-        if dep_args != &def_args {
-            bug!("[invariant] dependency type arguments mismatch on {def_desc}");
+
+        let mut ty_args_matched = true;
+        for (dep_arg, ty_arg) in dep_args.iter().zip(instance.args.iter()) {
+            let def_arg = temp_builder.mk_ty_arg(ty_arg);
+            if dep_arg != &def_arg {
+                ty_args_matched = false;
+                break;
+            }
+        }
+        if !ty_args_matched {
+            info!("- skipped, type arguments mismatch");
+            continue;
+        }
+
+        // try to match the instance kind
+        match (dep_kind, instance.def) {
+            (SolInstanceKind::Regular, InstanceKind::Item(_)) => (),
+
+            (SolInstanceKind::DropEmpty, InstanceKind::DropGlue(_, None)) => (),
+            (SolInstanceKind::DropGlued(dep_ty), InstanceKind::DropGlue(_, Some(ty))) => {
+                if dep_ty.as_ref() != &temp_builder.mk_type(ty) {
+                    info!("- skipped, drop glued type mismatch");
+                    continue;
+                }
+            }
+
+            (SolInstanceKind::VTableShim, InstanceKind::VTableShim { .. }) => (),
+            (SolInstanceKind::ReifyShim, InstanceKind::ReifyShim { .. }) => (),
+            (SolInstanceKind::ClosureOnceShim, InstanceKind::ClosureOnceShim { .. }) => (),
+            (SolInstanceKind::FnPtrShim(dep_ty), InstanceKind::FnPtrShim(_, ty)) => {
+                if dep_ty.as_ref() != &temp_builder.mk_type(ty) {
+                    info!("- skipped, fn ptr shim type mismatch");
+                    continue;
+                }
+            }
+            (SolInstanceKind::FnPtrAddrShim(dep_ty), InstanceKind::FnPtrAddrShim(_, ty)) => {
+                if dep_ty.as_ref() != &temp_builder.mk_type(ty) {
+                    info!("- skipped, fn ptr addr shim type mismatch");
+                    continue;
+                }
+            }
+            (SolInstanceKind::CloneShim(dep_ty), InstanceKind::CloneShim(_, ty)) => {
+                if dep_ty.as_ref() != &temp_builder.mk_type(ty) {
+                    info!("- skipped, clone shim type mismatch");
+                    continue;
+                }
+            }
+
+            _ => {
+                info!("- skipped, instance kind mismatch");
+                continue;
+            }
         }
 
         // end of the loop
@@ -167,11 +208,16 @@ pub(crate) fn phase_expansion<'tcx>(tcx: TyCtxt<'tcx>, sol: SolEnv, instance: In
         break;
     }
 
-    // serialize the context
-    if matched {
-        let (sol, context) = builder.build();
-        sol.serialize_to_file("context", &context);
-    } else {
+    // exit early if no dependency matched
+    if !matched {
         info!("- skipped, not in the dependency list");
+        return;
     }
+
+    // now resolve the context
+    let mut builder = SolContextBuilder::new(tcx, sol);
+    builder.make_instance(instance);
+
+    let (sol, context) = builder.build();
+    sol.serialize_to_file("context", &context);
 }
