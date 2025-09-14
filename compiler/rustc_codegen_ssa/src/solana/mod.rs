54210009caf6866fddfa285b0fae887e015a3880
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-09-14 12:31:02 -0400

    kickstart spl

--- compiler/rustc_codegen_ssa/src/solana/mod.rs
+++ compiler/rustc_codegen_ssa/src/solana/mod.rs
@@ -4,24 +4,32 @@ use rustc_middle::ty::{Instance, TyCtxt};
 pub(crate) mod common;
 pub(crate) mod context;
 pub(crate) mod pipeline_anchor;
+pub(crate) mod pipeline_shared;
+
+use common::{BuildSystem, Phase, retrieve_env};
 
 /// Entrypoint for the solana-specific codegen logic
 pub(crate) fn entrypoint<'tcx>(tcx: TyCtxt<'tcx>, instance: Instance<'tcx>) {
-    let sol = match common::retrieve_env(tcx) {
+    let sol = match retrieve_env(tcx) {
         None => return,
         Some(ctxt) => ctxt,
     };
 
     // work on the monomorphised MIR
-    match sol.build_system {
-        common::BuildSystem::Anchor => match sol.phase {
-            common::Phase::Bootstrap => {
-                pipeline_anchor::phase_bootstrap(tcx, sol, instance);
+    match sol.phase {
+        Phase::Bootstrap => {
+            match sol.build_system {
+                BuildSystem::Spl => {
+                    // TODO: add SPL-specific processing here
+                }
+                BuildSystem::Anchor => {
+                    pipeline_anchor::phase_bootstrap(tcx, sol, instance);
+                }
             }
-            common::Phase::Expansion(_) => {
-                pipeline_anchor::phase_expansion(tcx, sol, instance);
-            }
-            common::Phase::Temporary => bug!("[invariant] unexpected temporary phase"),
-        },
+        }
+        Phase::Expansion(_) => {
+            pipeline_shared::phase_expansion(tcx, sol, instance);
+        }
+        Phase::Temporary => bug!("[invariant] unexpected temporary phase"),
     }
 }
